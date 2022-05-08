dde4c2461df61847e52bb7b75f61c9314b7a6295
Esteban Mena <estebanmrj99@gmail.com>
2022-05-08 11:21:33 -0600

    arreglados datos

--- data.sql
+++ data.sql
@@ -8,12 +8,12 @@ values('CiudadTarea')
 ---     EL CUARTO ES ABAJO DERECHA, EL ULTIMO PUNTO DEBE CALZAR CON EL PRIMERO 
 
 insert into [dbo].[casa](idCiudad, numero, ubicacion)
-values(1, '#21-A', geometry::STGeomFromText('POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))', 4326)),
-	  (1, '#21-B', geometry::STGeomFromText('POLYGON((1 0, 1 1, 2 1, 2 0, 1 0))', 4326)), --- Horizontal
-	  (1, '#21-C', geometry::STGeomFromText('POLYGON((2 0, 2 1, 3 1, 3 0, 2 0))', 4326)),
-	  (1, '#21-D', geometry::STGeomFromText('POLYGON((3 0, 3 1, 4 1, 4 0, 3 0))', 4326)),
-	  (1, '#21-E', geometry::STGeomFromText('POLYGON((4 0, 4 1, 5 1, 5 0, 4 0))', 4326)),
-	  (1, '#21-F', geometry::STGeomFromText('POLYGON((5 0, 5 1, 6 1, 6 0, 5 0))', 4326))
+values(1, '#casa-A', geometry::STGeomFromText('POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))', 4326)), --- Horizontal
+	  (1, '#casa-B', geometry::STGeomFromText('POLYGON((1 0, 1 1, 2 1, 2 0, 1 0))', 4326)), --- Horizontal
+      (1, '#casa-C', geometry::STGeomFromText('POLYGON((4 0, 4 1, 5 1, 5 0, 4 0))', 4326)), --- Horizontal
+	  (1, '#casa-D', geometry::STGeomFromText('POLYGON((0 2, 0 3, 1 3, 1 2, 0 2))', 4326)), --- Vertical
+	  (1, '#casa-E', geometry::STGeomFromText('POLYGON((0 3, 0 4, 1 4, 1 3, 0 3))', 4326)), --- Vertical
+      (1, '#casa-E', geometry::STGeomFromText('POLYGON((0 4, 0 5, 1 5, 1 4, 0 4))', 4326)) --- Vertical
 
 
 insert into [dbo].[calle](idCiudad, numero, ubicacion)
@@ -26,10 +26,10 @@ values('#tipo1'),
 
 insert into [dbo].[comercio](idTipo, idCiudad, nombre, numero, ubicacion)
 values(1, 1, 'Comercio1', '#comercio-A', geometry::STGeomFromText('POLYGON((0 1, 0 2, 1 2, 1 1, 0 1))', 4326)), --- Vertical
-	  (1, 1, 'Comercio2', '#comercio-B', geometry::STGeomFromText('POLYGON((0 2, 0 3, 1 3, 1 2, 0 2))', 4326)),
-	  (1, 1, 'Comercio3', '#comercio-C', geometry::STGeomFromText('POLYGON((0 3, 0 4, 1 4, 1 3, 0 3))', 4326)),
-	  (2, 1, 'Comercio4', '#comercio-D', geometry::STGeomFromText('POLYGON((0 4, 0 5, 1 5, 1 4, 0 4))', 4326)),
-	  (2, 1, 'Comercio5', '#comercio-E', geometry::STGeomFromText('POLYGON((0 5, 0 6, 1 6, 1 5, 0 5))', 4326))
+      (1, 1, 'Comercio2', '#comercio-B', geometry::STGeomFromText('POLYGON((2 0, 2 1, 3 1, 3 0, 2 0))', 4326)), --- Horizontal
+      (1, 1, 'Comercio3', '#comercio-C', geometry::STGeomFromText('POLYGON((3 0, 3 1, 4 1, 4 0, 3 0))', 4326)), --- Horizontal
+	  (2, 1, 'Comercio4', '#comercio-D', geometry::STGeomFromText('POLYGON((5 0, 5 1, 6 1, 6 0, 5 0))', 4326)), --- Horizontal
+	  (2, 1, 'Comercio5', '#comercio-E', geometry::STGeomFromText('POLYGON((0 5, 0 6, 1 6, 1 5, 0 5))', 4326))  --- Vertical
 
 
 insert into dbo.producto(nombre)
